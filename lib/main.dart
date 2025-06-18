1473af68858e13a62139bee2fc2945652ada70ef
JingSongJai <chingsong57@gmail.com>
2025-06-18 22:23:26 +0700

    change hash to path url

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:get/get.dart';
 import 'package:portfolio/app_page/app_page.dart';
 import 'package:portfolio/language/app_language.dart';
@@ -8,6 +9,7 @@ import 'package:portfolio/view/mainscreen/main_screen.dart';
 import 'package:portfolio/view/mainscreen/mainscreen_bindings.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   runApp(const MainApp());
 }
 
